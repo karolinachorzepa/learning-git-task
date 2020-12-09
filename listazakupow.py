4dfd3c1e08a214d4523fbc4dd7a9be9e28777664
Karolina Chorzepa <karolina.chorzepa@o2.pl>
2020-12-09 20:51:52 +0100

    Branch_greetings

--- listazakupow.py
+++ listazakupow.py
@@ -4,7 +4,9 @@ list1= {"piekarnia":["chleb", "bułka", "pączek"],"warzywniak" : ["rukola", "se
 for c,b in list1.items():
   print(f"Idę do {c.capitalize()} i kupuję {b}")
 
+
 articles = {"chleb", "bułka", "pączek","rukola", "seler", "pomidor"}
 
 c=len(articles)
 print(f"W sumie kupuję: {c} produktów")
+print("Pozdrowienia z Krakowa ;)")

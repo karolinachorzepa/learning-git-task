112e7aa171d6aa9513c344ad8d2e27a876dd80bc
Karolina Chorzepa <karolina.chorzepa@o2.pl>
2020-12-09 20:05:49 +0100

    Init project

---
+++ listazakupow.py
@@ -0,0 +1,13 @@
+a=("lista zakupów:")
+print(a.capitalize())
+list1= {"piekarnia":["chleb", "bułka", "pączek"],"warzywniak" : ["rukola", "seler", "pomidor"]}
+
+
+for c,b in list1.items():
+  print(f"Idę do {c.capitalize()} i kupuję {b}")
+
+articles = {"chleb", "bułka", "pączek","rukola", "seler", "pomidor"}
+
+c=len(articles)
+print(c)
+print(f"W sumie kupuję: {c} produktów")
